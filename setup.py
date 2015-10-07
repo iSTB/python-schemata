58e349f221f868e8d53b89fad3dd3301fa4efb76
jmckayfletcher <jmckayfletcher@ubuntu.(none)>
2015-10-07 18:45:33 +0100

    more cleaning

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 # -*- coding:utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'schematax',
-    version = '0.1',
+    version = '0.1.2',
     author = 'Jack Fletcher',
     author_email = 'jack.mckayfletcher@plymouth.ac.uk',
-    packages = ['schematax'],
+    packages=find_packages(),
     url = 'https://github.com/iSTB/python-schemata',
-    license = 'LICENSE',
+    license = 'LICENSE.txt',
     keywords=['schemata', 'schema', 'genetic algorithms', 'GAs', 'complete lattice', 'meet', 'join'],
     description = 'A python package for all things schemata related',
     long_description = open('README.rst').read(),
