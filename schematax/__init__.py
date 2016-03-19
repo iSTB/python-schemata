4e13a86bb23ba353d6ce0b59127cce0495fe4b79
jmckayfletcher <jmckayfletcher@ubuntu.(none)>
2016-03-19 14:55:42 +0000

    updating setup and innit files

--- schematax/__init__.py
+++ schematax/__init__.py
@@ -2,8 +2,9 @@
 A python class to deal with all things schemata.
 """
 
-from .schemata import comparable, infimum, schema, complete, is_lower_n, draw, join, supremum, get_lower_ns, meet 
-__all__ =  ['comparable', 'infimum', 'schema', 'complete', 'is_lower_n', 'draw', 'join', 'supremum', 'get_lower_ns', 'meet']
+from schemata import comparable, infimum, schema, complete, is_lower_n, draw, join, supremum, get_lower_ns, meet, complete_naive,complete_fast
+__all__ =  ['comparable', 'infimum', 'schema', 'complete', 'is_lower_n', 'draw', 'join', 'supremum', 'get_lower_ns', 'meet','compete_naive','complete_fast']
  
+
 
 
--- setup.py
+++ setup.py
@@ -2,6 +2,29 @@
 from setuptools import setup, find_packages
 import io, os
 from rst_cleaner import yield_sphinx_only_markup
+#from Cython.Build import cythonize
+from distutils.extension import Extension
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+cmdclass = { }
+ext_modules = [ ]
+
+if use_cython:
+    ext_modules += [
+        Extension("schematax.schemata", [ "schematax/schemata.pyx" ]),
+    ]
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules += [
+        Extension("schematax.schemata", [ "schematax/schemata.pyx" ]),
+    ]
+
+
 
 def read_text_lines(fname):
     with io.open(os.path.join('.', fname)) as fd:
@@ -12,8 +35,10 @@ readme_lines = read_text_lines('README.rst')
 
 setup(
     name = 'schematax',
-    version = '0.1.6',
+    version = '0.1.7',
     author = 'Jack Fletcher',
+    cmdclass = cmdclass,
+    ext_modules=ext_modules,
     author_email = 'jack.mckayfletcher@plymouth.ac.uk',
     packages=find_packages(),
     url = 'https://github.com/iSTB/python-schemata',
@@ -21,5 +46,5 @@ setup(
     keywords=['schemata', 'schema', 'genetic algorithms', 'GAs', 'complete lattice', 'meet', 'join'],
     description = 'A python package for all things schemata related',
     long_description = ''.join(yield_sphinx_only_markup(readme_lines)),
-    install_requires=['graphviz'],
+    install_requires=['graphviz','numpy'],
 )
